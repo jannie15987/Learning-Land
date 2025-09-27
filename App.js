ef66c619bc7b50cc47fe46cd13e6b667f0963fa3
jannie15987 <janicerodrick@gmail.com>
2025-09-27 18:17:45 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,1 @@
-// src/App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ReadingLanding from "./pages/reading/ReadingLanding";
-import Level1AlphabetGame from "./pages/reading/Level1AlphabetGame";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/reading" element={<ReadingLanding />} />
-        <Route path="/reading/level1" element={<Level1AlphabetGame />} />
-        {/* Add more levels/routes here */}
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
